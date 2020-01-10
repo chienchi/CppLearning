81f5555988288dc0ec8c6cfa350d6e732564b666
osboxes <chienchi@lanl.gov>
2020-01-09 23:36:33 -0700

    update

--- MandelbrotSet/main.cpp
+++ MandelbrotSet/main.cpp
@@ -21,16 +21,18 @@ int main (int argc, char* argv[]) {
     //auto Juliaset = z_plane | xform([](){ return z*z + k; })
 
    // auto newRange = Xrange(xdim) | Xrange(ydim);
-     auto println = for_each([&,xdim](auto i) {  i.first == (xdim - 1)? std::cout<<"\n": std::cout << i.second ; });
+     auto println = for_each([xdim](auto i) {  i.first == (xdim - 1)? std::cout<<"\n": std::cout << i.second ; });
      auto ij = rectangle({0,0},{xdim,ydim});
-     auto z_plane = ij | [=](auto i){
+     auto z_plane = [&](auto i){
          complex<float> c (i.second,i.first);
          c*= 2.4f / static_cast<float>(ydim);
          c-= complex<float>(1.2 * xdim / ydim + 0.5, 1.2);
+         //store x axis and complex number in pair
+        // cout << c << " ";
          return std::pair<int,complex<float>> {i.second, c};;
      };
 
-     auto mendelbrot = [=](auto c){
+     auto mendelbrot = [&](auto c){
          complex<float> oc = c.second;
          complex<float> z = c.second;
          int iter;
@@ -38,29 +40,26 @@ int main (int argc, char* argv[]) {
          for ( iter = 0 ; iter < max_iter && norm(z) <= 4.0f; iter++) {
              z = z * z + oc;
          }
-         //cout<<c.first << " ";
-
          return iter==max_iter? std::pair<int,char> {c.first, '#'}:std::pair<int,char> {c.first,' '};
      };
 
-    z_plane | mendelbrot | println;
+    cout << "Mendelbrot" << endl;
+    ij | z_plane | mendelbrot | println;
 
-    auto Juliaset = [=](auto c){
+    auto Juliaset = [&](auto c){
         complex<float> z = c.second;
         //complex<float> k(0.353,0.288);
         complex<float> k (-0.6,0.6);
         int iter;
-        //#pragma omp parallel for default(none) shared(z)
+        #pragma omp parallel for default(none) shared(z)
         for ( iter = 0 ; iter < max_iter && norm(z) <= 4.0f; iter++) {
             z = z * z + k;
         }
-        //cout << iter << " ";
         return iter==max_iter? std::pair<int,char> {c.first, '#'}:std::pair<int,char> {c.first,' '};
-        //return iter==max_iter? "#":" ";
     };
 
     cout << "Juliaset" << endl;
-    z_plane | Juliaset | println;
+    //ij| z_plane | Juliaset | println;
 
      auto z = for_each([=](auto i) {
         // for (auto j:i){
